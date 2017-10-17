9a3f1fee9781850d40c66d3fdcb7c8c04eae93d6
kissmygritts <kissmygritts08@gmail.com>
2017-10-17 11:27:12 -0700

    limit animal_card to 250 elements for now

--- db/sql/elements/all.sql
+++ db/sql/elements/all.sql
@@ -7,6 +7,8 @@ projects.proj_name AS project,
 events.status,
 elements.sex,
 events.age,
+events.x,
+events.y,
 events.comments,
 species.grouping,
 (
@@ -31,8 +33,55 @@ species.grouping,
   WHERE event_id = events.id
 ) AS "labids"
 -- (qaqc)
--- locs)
+-- (locs)
 FROM elements
   INNER JOIN species ON elements.species_id = species.id
   LEFT JOIN events ON elements.id = events.element_id
-  LEFT JOIN projects ON events.project_id = projects.id;
+  LEFT JOIN projects ON events.project_id = projects.id
+ORDER BY date DESC, ndow_id
+LIMIT 250;
+
+-- WITH m AS (
+--   SELECT element_id, mark_id, mark_color, mark_type, mark_location, date_given, date_removed
+--   FROM marks
+-- ),
+
+-- d AS (
+--   SELECT element_id, serial_num, frequency, type, inservice, outservice
+--   FROM deployments
+-- ),
+
+-- l AS (
+--   SELECT event_id, lab_id
+--   FROM lab_ids
+-- )
+
+-- SELECT
+--   elements.id,
+--   species.common_name as species,
+--   elements.animal_id as ndow_id,
+--   events.event_date AS date,
+--   events.status,
+--   elements.sex,
+--   events.age,
+--   events.comments,
+--   species.grouping,
+--   (
+--     SELECT to_jsonb(array_agg(to_jsonb(m)))
+--     FROM m
+--     WHERE element_id = elements.id
+--   ) AS "marks",
+--   (
+--     SELECT to_jsonb(array_agg(to_jsonb(d)))
+--     FROM d
+--     WHERE element_id = elements.id
+--   ) AS "devices",
+--   (
+--     SELECT to_jsonb(array_agg(to_jsonb(lab_id)))
+--     FROM l
+--     WHERE event_id = events.id
+--   ) AS "labids"
+-- FROM elements
+--   INNER JOIN species ON elements.species_id = species.id
+--   LEFT JOIN events ON elements.id = events.element_id
+-- ORDER BY date DESC, ndow_id;
